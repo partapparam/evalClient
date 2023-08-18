edd34c4ad8390ae66d3bac362b9b9d2841623965
partapparam <paramsingh1961@gmail.com>
2023-08-17 22:41:16 -0700

    aws sns

--- src/features/auth/ForgotPasswordForm.jsx
+++ src/features/auth/ForgotPasswordForm.jsx
@@ -1,14 +1,19 @@
 import React from "react"
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export const ForgotPasswordForm = () => {
-  const [email, setEmail] = useState()
+  const [email, setEmail] = useState("")
+  const navigate = useNavigate()
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    setEmail(event.target.value)
     console.log(email)
-    console.log(event.target)
+  }
+
+  const handleCancel = () => {
+    setEmail("")
+    navigate("/")
   }
 
   return (
@@ -22,27 +27,29 @@ export const ForgotPasswordForm = () => {
           </div>
           <form className="mt-8 space-y-6 text-left" onSubmit={handleSubmit}>
             <div className="-space-y-px rounded-md shadow-sm">
-              <div>
-                <label htmlFor="email" className="sr-only">
-                  Enter your email
-                </label>
-                <input
-                  id="email"
-                  required
-                  name="email"
-                  type="email"
-                  className="relative block w-full rounded-md border-0 py-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6 px-3 outline-2 outline outline-gray-300 focus:outline-blue-600 hover:outline-blue-600"
-                  placeholder="Email"
-                />
-              </div>
+              <label htmlFor="email" className="sr-only">
+                Enter your email
+              </label>
+              <input
+                onChange={(event) => setEmail(event.target.value)}
+                id="email"
+                required
+                name="email"
+                type="email"
+                value={email || ""}
+                className="relative block w-full rounded-md border-0 py-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6 px-3 outline-2 outline outline-gray-300 focus:outline-blue-600 hover:outline-blue-600"
+                placeholder="Email"
+              />
             </div>
             <div className="flex justify-end">
               <button type="button" className="btn-secondary m-2">
                 Cancel
               </button>
-              <button type="submit" className="btn-primary my-2">
-                Send
-              </button>
+              <input
+                type="submit"
+                value="Submit"
+                className="btn-primary my-2"
+              />
             </div>
           </form>
         </div>
