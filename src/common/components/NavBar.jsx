007f5379d16bc06b2baaa285603f8f93a340f3d1
partapparam <paramsingh1961@gmail.com>
2023-08-29 18:04:05 +0100

    ui

--- src/common/components/NavBar.jsx
+++ src/common/components/NavBar.jsx
@@ -69,7 +69,7 @@ export const NavBar = () => {
 
                         <Link to="/signup">
                           <p className="text-slate-100 hover:bg-violet-400 transition duration-300 rounded-md px-3 py-2 text-sm font-medium">
-                            Signup
+                            Sign up
                           </p>
                         </Link>
                       </>
@@ -113,7 +113,7 @@ export const NavBar = () => {
                     </Disclosure.Button>
                     <Disclosure.Button as={Link} to="/signup">
                       <p className="text-slate-100 hover:bg-violet-400 transition duration-300 rounded-md px-3 py-2 text-sm font-medium">
-                        Signup
+                        Sign up
                       </p>
                     </Disclosure.Button>
                   </>
