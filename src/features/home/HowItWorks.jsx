5f7f82c4ee42c65bf2cbf0dd2fbc11364dbb72f4
partapparam <paramsingh1961@gmail.com>
2023-09-20 10:26:13 -0700

    How it works

---
+++ src/features/home/HowItWorks.jsx
@@ -0,0 +1,4 @@
+import React from "react"
+import { HIWList } from "./howItWorksList"
+
+export const HowItWorks = () => {}
---
+++ src/features/home/howItWorksList.js
@@ -0,0 +1,18 @@
+export const HIWList = [
+  {
+    title: "Sign up for Eval",
+    content: `You must have an account to leave reviews`,
+  },
+  {
+    title: "Search for an Address",
+    content: `Look up the address of your job. `,
+  },
+  {
+    title: "Choose the resident",
+    content: `Choose the resident you want to review or create one if they don't exist at this address`,
+  },
+  {
+    title: "Leave a review",
+    content: `Its important to leave an honest review. It will be public an monitored by our team.`,
+  },
+]
